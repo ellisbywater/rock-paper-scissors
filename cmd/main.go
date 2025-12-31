f304de49cf3a414fbf98b8ea038f8c86e836bb27
Ellis Garaudy <egaraudy@gmail.com>
2025-12-31 08:32:05 -0600

    consolidated router to main

--- cmd/main.go
+++ cmd/main.go
@@ -6,15 +6,63 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ellisbywater/http-rock-paper-scissors/internal/domain"
+	"github.com/ellisbywater/http-rock-paper-scissors/internal/handler"
+	"github.com/ellisbywater/http-rock-paper-scissors/internal/repository"
+	"github.com/ellisbywater/http-rock-paper-scissors/internal/service"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
+
+func buildGameHandlerDeps(db *sql.DB) handler.GameHandlers {
+	var gameRepo domain.GameRepository = repository.NewGameRepository(db)
+	var gameService service.GameService = *service.NewGameService(gameRepo)
+	return *handler.NewGameHandler(gameService)
+}
+
+func buildPlayerHandlerDeps(db *sql.DB) handler.PlayerHandlers {
+	var playerRepo domain.PlayerRepository = repository.NewPlayerRepository(db)
+	var playerService service.PlayerService = *service.NewPlayerService(playerRepo)
+	return *handler.NewPlayerHandler(playerService)
+}
+
+func buildRoundHandlerDeps(db *sql.DB) handler.RoundHandlers {
+	var roundRepo domain.RoundRepository = repository.NewRoundRepository(db)
+	var roundService service.RoundService = *service.NewRoundService(roundRepo)
+	return *handler.NewRoundHandlers(roundService)
+}
+
+func NewRouterWithDeps(db *sql.DB) *http.ServeMux {
+	r := http.NewServeMux()
+
+	gameHandler := buildGameHandlerDeps(db)
+	playerHandler := buildPlayerHandlerDeps(db)
+	roundHandler := buildRoundHandlerDeps(db)
+
+	r.HandleFunc("POST /player/create", playerHandler.Create)
+	r.HandleFunc("GET /player/{playerId}", playerHandler.Get)
+	r.HandleFunc("GET /player/{playerId}/games", playerHandler.GetGames)
+
+	r.HandleFunc("POST /game/create", gameHandler.Create)
+	r.HandleFunc("GET /game/{gameId}", gameHandler.GetGame)
+
+	r.HandleFunc("POST /round/create", roundHandler.Create)
+
+	return r
+}
 
 type App struct {
 	Port   string
 	Router *http.ServeMux
 }
 
+const port = ":8080"
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
@@ -25,13 +73,23 @@ func main() {
 		log.Fatalf("error in connection with database %s", err)
 	}
 
+	r := http.NewServeMux()
+
+	gameHandler := buildGameHandlerDeps(db)
+	playerHandler := buildPlayerHandlerDeps(db)
+	roundHandler := buildRoundHandlerDeps(db)
+
+	r.HandleFunc("POST /player/create", playerHandler.Create)
+	r.HandleFunc("GET /player/{playerId}", playerHandler.Get)
+	r.HandleFunc("GET /player/{playerId}/games", playerHandler.GetGames)
+
+	r.HandleFunc("POST /game/create", gameHandler.Create)
+	r.HandleFunc("GET /game/{gameId}", gameHandler.GetGame)
+
+	r.HandleFunc("POST /round/create", roundHandler.Create)
+
 	log.Println("Connected to database")
 
-	app := App{
-		Port:   ":8080",
-		Router: NewRouterWithDeps(db),
-	}
-
-	log.Println("Rock Paper Scissors running on Port ", app.Port)
-	log.Fatal(http.ListenAndServe(app.Port, app.Router))
+	log.Println("Rock Paper Scissors running on Port ", port)
+	log.Fatal(http.ListenAndServe(port, r))
 }
